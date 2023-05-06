9f79015c12b5d279e8df7fa69855a46de1ad2602
ajaysathvik <111119810+ajaysathvik@users.noreply.github.com>
2023-05-06 15:44:34 +0530

    Create IPFS.sol

---
+++ IPFS.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract IPFSTransfer {
+    mapping(address => string) public ipfsAddresses;
+
+    function transferIPFS(address recipient, string memory cid) external {
+        ipfsAddresses[recipient] = cid;
+    }
+}
